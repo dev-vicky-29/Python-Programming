3815b8ebe4832f367a2309e71a803f6fa409215f
dev-vicky-29 <vickydeshmukh2509@gmail.com>
2024-05-06 22:55:22 +0530

    Create TrafficLightCondition.py

---
+++ Apna College/TrafficLightCondition.py
@@ -0,0 +1,20 @@
+"""
+3 Colors
+Red:"Stop"
+Green:"Go"
+Yellow:"Look"
+Blue,White:"Light is Broken"
+Else:Not Valid Keyword
+"""
+
+light=input("Enter any Traffic Light:")
+if (light=="Red"):
+    print("Stop!")
+elif (light=="Green"):
+    print("Go!")
+elif (light=="Yellow"):
+    print("Look!")
+elif (light=="Blue"):
+    print("Light is Broken")
+else:
+    print("Invalid Keyword, Please try Again Later")
